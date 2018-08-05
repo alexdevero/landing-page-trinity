ee13c781e8be353b0c5c9e47825752afbbf61fcb
Alex Devero <deveroalex@gmail.com>
2018-08-05 10:02:29 +0200

    Add gulpfile.babel.js

---
+++ gulpfile.babel.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import gulp from 'gulp'
+
+import requireDir from 'require-dir'
+
+requireDir('./gulpfile/')
+
+// const clean = (cb) => {
+//   cb()
+// }
+
+// const build = (cb) => {
+//   cb()
+// }
+
+// const defaultTask = (cb) => {
+//   cb()
+// }
+
+// exports.default = defaultTask
+// exports.default = series()
