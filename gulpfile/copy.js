3effe604db86ace0fb962416d3673adeb4ebaca8
Alex Devero <deveroalex@gmail.com>
2018-08-14 10:14:17 +0200

    Fix and clean up copy tasks

--- gulpfile/copy.js
+++ gulpfile/copy.js
@@ -6,54 +6,25 @@ import plumber from 'gulp-plumber'
 import prune from 'gulp-prune'
 
 // Copy CSS files
-gulp.task('copy:css', () => {
+gulp.task('copy:css', (done) => {
   const cssPath = './dist/styles'
 
-  return gulp.src(['./src/styles/_vendor/*', './node_modules/modern-normalize/modern-normalize.css'])
+  gulp.src(['./src/styles/_vendor/*', './node_modules/modern-normalize/modern-normalize.css'])
     .pipe(plumber())
     .pipe(prune(cssPath))
     .pipe(changed(cssPath))
     .pipe(gulp.dest(cssPath))
+  done()
 })
 
 // Copy font files
-gulp.task('copy:fonts', () => {
+gulp.task('copy:fonts', (done) => {
   const fontsPath = './dist/fonts'
 
-  return gulp.src('./src/fonts/*')
+  gulp.src('./src/fonts/*')
     .pipe(plumber())
     .pipe(prune(fontsPath))
     .pipe(changed(fontsPath))
     .pipe(gulp.dest(fontsPath))
-})
-
-// Copy JS plugins files
-gulp.task('copy:jsplugins', () => {
-  const jsPluginsPath = './dist/js/plugins/'
-
-  return gulp.src([
-    './src/js/plugins/*.js',
-    './node_modules/vanilla-lazyload/dist/lazyload.min.js',
-  ])
-    .pipe(plumber())
-    .pipe(prune({ dest: jsPluginsPath, ext: ['.js'] }))
-    .pipe(changed(jsPluginsPath))
-    .pipe(gulp.dest(jsPluginsPath))
-})
-
-// Copy other files
-gulp.task('copy:other', () => {
-  const otherPath = './dist/'
-
-  return gulp.src([
-    './src/.htaccess',
-    './src/contact.php',
-    './src/crossdomain.xml',
-    './src/humans.txt',
-    './src/robots.txt'
-  ])
-    .pipe(plumber())
-    .pipe(prune({ dest: otherPath, ext: ['.htaccess', '.php', '.txt', '.xml'] }))
-    .pipe(changed(otherPath))
-    .pipe(gulp.dest(otherPath))
+  done()
 })
