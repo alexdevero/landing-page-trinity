f7d50efd0f958e3ae77d26e4d17219282370d187
Alex Devero <deveroalex@gmail.com>
2018-08-14 11:57:11 +0200

    Finish Gulp setup

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,17 +6,27 @@ import requireDir from 'require-dir'
 
 requireDir('./gulpfile/')
 
-// const clean = (cb) => {
-//   cb()
-// }
+const environment = process.env.NODE_ENV
+if (environment !== undefined) {
+  console.log(`Environment: ${environment}`)
+}
 
-// const build = (cb) => {
-//   cb()
-// }
+// Automate copying
+gulp.task('copy:all', gulp.series('copy:css', 'copy:fonts'))
 
-// const defaultTask = (cb) => {
-//   cb()
-// }
+// Builds the website
+gulp.task('build:dev', gulp.series('copy:all', 'html', 'images', 'css', 'js'))
 
-// exports.default = defaultTask
-// exports.default = series()
+// Test task for testing HTML, Sass and JavaScript
+gulp.task('test', gulp.series('html:test', 'css:test', 'js:test'))
+
+// Watch HTML, CSS and JavaScript files for changes
+gulp.task('watch', () => {
+  gulp.watch('./src/*.html', gulp.parallel('html'))
+  gulp.watch('./src/styles/**/*.css', gulp.parallel('css'))
+  gulp.watch('./src/js/**/*.js', gulp.parallel('js'))
+  gulp.watch(['./src/images/**/*', '!./src/images/**/*.rar'], gulp.parallel('images'))
+})
+
+// Default task - build website, watch HTML, CSS and JavaScript files for changes and start server
+gulp.task('default', gulp.series('build:dev', gulp.parallel('watch', 'browser-sync')))
